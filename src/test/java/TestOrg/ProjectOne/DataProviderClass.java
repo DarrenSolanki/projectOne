a1fb24027e4fd2e89d05918199db91d1f9b1ddba
Darren <dharm.09@gmail.com>
2026-04-10 22:10:17 +0530

    data provider test

---
+++ src/test/java/TestOrg/ProjectOne/DataProviderClass.java
@@ -0,0 +1,29 @@
+package TestOrg.ProjectOne;
+import org.testng.annotations.*;
+
+public class DataProviderClass {
+
+	@DataProvider
+	public Object getData() {
+		Object[][] data = new Object[3][2];
+		
+		data[0][0]="user one";
+		data[0][1]="password one";
+		
+		data[1][0]="user two";
+		data[1][1]="password two";
+		
+		data[2][0]="user three";
+		data[2][1]="password three";
+		
+		return data;
+	}
+	
+	@Test(dataProvider = "getData")
+	public void printData(String user, String password) {
+		System.out.println("User : "+ user);
+		System.out.println("Password : "+ password);
+
+	}
+
+}
