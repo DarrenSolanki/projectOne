d75965e089e551f57a1ae8b88a44a8f2bfff0c58
Darren <dharm.09@gmail.com>
2026-04-10 10:02:31 +0530

    retry browser launch

--- src/test/java/TestOrg/ProjectOne/AppTest.java
+++ src/test/java/TestOrg/ProjectOne/AppTest.java
@@ -40,7 +40,7 @@ public class AppTest {
     	WebDriver driver = new ChromeDriver();
     	driver.get("http://google.com");
     	Thread.sleep(5000);
-    	driver.close();
+    	
     	
     	
     }
